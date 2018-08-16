e3a126f40823aba144f90f8bb2ab4373cc118730
buguyizu <yahveh.chen@gmail.com>
2018-08-16 15:33:40 +0800

    java8, sse, pom

--- p1/src/main/java/info/yinhua/web/jetty/UserEventSourceServlet.java
+++ p1/src/main/java/info/yinhua/web/jetty/UserEventSourceServlet.java
@@ -1,15 +1,18 @@
 package info.yinhua.web.jetty;
 
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 
-//https://github.com/jetty-project/jetty-eventsource-servlet/wiki
-//https://github.com/alexcheng1982/server-sent-events-sample
-//https://github.com/marschall/event-source-sample
+// 原本地在web.xml中配置的
+// https://github.com/jetty-project/jetty-eventsource-servlet/wiki
+// https://github.com/alexcheng1982/server-sent-events-sample
+// https://github.com/marschall/event-source-sample
 @WebServlet(asyncSupported = true, value = "/sse/*")
+@WebFilter(asyncSupported = true, value = "/sse/*")
 public class UserEventSourceServlet extends EventSourceServlet {
-	
-    /**
+
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
@@ -21,9 +24,8 @@ public class UserEventSourceServlet extends EventSourceServlet {
 		//因为servlet不是spring创建的，所以只有通过这种方式(WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext()))得到sessionRegistry，通过注入不行。
 //		ApplicationContext context = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());
 //		SessionRegistry sessionRegistry = (SessionRegistry) context.getBean("sessionRegistry");
-		
+
 		UserEventSource eventSource = new UserEventSource(this, request);
-		
 		return eventSource;
 	}
 }
