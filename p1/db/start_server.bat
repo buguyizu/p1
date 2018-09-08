5a6f4017373819c7a19f5713b6e5627c042ca82a
buguyizu <yahveh.chen@gmail.com>
2018-09-08 12:40:21 +0800

    maven.plugins

--- p1/db/start_server.bat
+++ p1/db/start_server.bat
@@ -5,7 +5,7 @@ rem 2017/05/12 modify
 rem h2*.jar to h2-1.3.176.jar
 
 cd .
-::jdbc:h2:tcp://localhost/db_namem
+::jdbc:h2:tcp://localhost/db_name
 ::sa 123
 ::origin password is null
 java -cp h2-1.3.176.jar org.h2.tools.Server -baseDir . -ifExists -tcp -tcpAllowOthers -pg -pgAllowOthers -web -webAllowOthers
